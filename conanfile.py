1c875100db8f4d12f5bb1881f6aad95b2806983f
Monkeber <27787571+monkeber@users.noreply.github.com>
2026-02-02 18:42:31 +0200

    Remove unused dependencies and use system installed qt

--- conanfile.py
+++ conanfile.py
@@ -15,10 +15,7 @@ class CS2ServerGUI(ConanFile):
         self.requires("nlohmann_json/3.11.3")
         self.requires("rapidcsv/8.84")
         self.requires("libcurl/8.17.0")
-        self.requires("m4/1.4.20", override=True)
-        self.requires("libb2/20190723")
-        self.requires("harfbuzz/12.3.0")
-        self.requires("qt/6.10.1")
+        self.requires("sqlitecpp/3.3.3")
 
     def layout(self):
         cmake_layout(self)
@@ -26,36 +23,3 @@ class CS2ServerGUI(ConanFile):
     def configure(self):
         if self.settings.os == "Windows": 
             self.options["libcurl"].with_ssl = "schannel"
-
-        self.options["harfbuzz"].with_glib = False
-
-        self.options["qt"].shared = True
-
-        self.options["qt"].openssl = True
-        self.options["qt"].with_glib = False
-        self.options["qt"].with_doubleconversion = True
-        self.options["qt"].with_freetype = True
-        self.options["qt"].with_fontconfig = True
-        self.options["qt"].with_icu = True
-        self.options["qt"].with_harfbuzz = True
-        self.options["qt"].with_libjpeg = False
-        self.options["qt"].with_libpng = True
-        self.options["qt"].with_sqlite3 = False
-        self.options["qt"].with_mysql = False
-        self.options["qt"].with_pq = False
-        self.options["qt"].with_odbc = False
-        self.options["qt"].with_zstd = False
-        self.options["qt"].with_brotli = False
-        self.options["qt"].with_dbus = True
-        self.options["qt"].with_libalsa = False
-        self.options["qt"].with_openal = False
-        self.options["qt"].with_gstreamer = False
-        self.options["qt"].with_pulseaudio = False
-        self.options["qt"].with_gssapi = False
-        self.options["qt"].with_md4c = False
-        self.options["qt"].with_x11 = True
-        self.options["qt"].with_egl = True
-
-        self.options["qt"].qtquick3d = True
-        self.options["qt"].qtshadertools = True
-        self.options["qt"].qtwayland = True
